1a3df75fe4e94ab9a72aaf094f9aa5c2120e10b4
Chiahsuan Su <S0395140@salemstate.edu>
2024-02-01 23:31:49 -0500

    Add

--- backend/server/routes/eventGetAllEvents.js
+++ backend/server/routes/eventGetAllEvents.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('./models/EventModel');
+const Event = require('../models/EventModel');
 
 // Get all events
 router.get('/all', async (req, res) => {
