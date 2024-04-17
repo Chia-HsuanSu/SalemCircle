d7685e5137f8d8c6ec392dc36c6b13609ddc09b9
Chiahsuan Su <S0395140@salemstate.edu>
2024-04-17 11:56:47 -0400

    labding

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const LandingPage = () => {
+const LandingPage = ({ user }) => {
     return (
-        <div className="home-container" style={{ backgroundColor: 'white', height: '100vh', marginTop: '50px'  }}>
+        <div className="home-container" style={{ backgroundColor: 'white', height: '100vh', marginTop: '50px' }}>
             <div className="header">
                 <div className="top-buttons" style={{ display: 'flex', justifyContent: 'center' }}>
                     <input type="text" placeholder="Search Events" className="search-bar" style={{ marginRight: '10px', height: '50px' }} />
-                    
                     <img src="/logo.png" alt="Logo" className="logo" width="200" height="100" style={{ marginRight: '10px' }} />
                     <Link to="/signup" style={{ marginRight: '10px', textDecoration: 'none' }}>
                         <button style={{ width: '100px', height: '50px' }}>Sign Up</button>
@@ -22,15 +21,17 @@ const LandingPage = () => {
             </div>
             {/* Salem Circle Description */}
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <div style={{ flex: 1, marginLeft: '200px'}}>
+                <div style={{ flex: 1, marginLeft: '200px' }}>
                     <p style={{ textAlign: 'left', marginBottom: '20px' }}>
                         Salem, famous for its historical significance, draws countless visitors yearly. Our app, Salem Circle, aims to enhance the city experience. It will feature "Events" - social gatherings for entertainment, exploration, and community engagement.
                     </p>
+                    {!user && (
+                        <p style={{ color: 'red' }}>Please sign up or log in to join events.</p>
+                    )}
                 </div>
                 <div style={{ flex: 1 }}>
                     <img src="/salem.png" alt="Salem" className="salem" width="300" height="400" />
                 </div>
-                
             </div>
         </div>
     );
